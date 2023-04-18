05291fef7d7cad464c4b12ed5efe066e6e5f04f3
tg136 <129951112+tg136@users.noreply.github.com>
2023-04-18 11:06:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
-streamlit.header("Fruityvice Fruit Advice!")
+
 
 
