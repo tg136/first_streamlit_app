fc138ad214cffac2b74fa304fe71b7f6a841176b
tg136 <129951112+tg136@users.noreply.github.com>
2023-04-20 10:33:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,5 @@ streamlit.dataframe(my_data_rows)
 #Add second fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add')
 streamlit.write('The user entered ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlt')")
